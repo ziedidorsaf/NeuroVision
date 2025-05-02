30d49445fd9dd072b8507decdcc80170ae4bcab9
ziedi dorsaf <166756130+ziedidorsaf@users.noreply.github.com>
2025-05-02 12:33:19 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,25 @@
+from datetime import datetime
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(50), nullable=True)
+    image = db.Column(db.String(200), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+   
+class Doctor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    contact_number = db.Column(db.String(15), nullable=False)
+    location_lat = db.Column(db.Float, nullable=False)
+    location_long = db.Column(db.Float, nullable=False)
+
 
