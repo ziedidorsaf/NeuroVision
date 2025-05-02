629b9b9c48b01f4c7f04536e97b7156f3ac85039
ziedi dorsaf <166756130+ziedidorsaf@users.noreply.github.com>
2025-05-02 13:18:41 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,5 +1,5 @@
 from app import create_app, db
-from app.models import User, Doctor  # Import the Doctor model
+from app.models import User, Doctor  
 
 
 # Create the Flask app
@@ -8,7 +8,7 @@ app = create_app()
 
 # Initialize the database within the app context
 with app.app_context():
-    # Drop all tables (optional, use with caution)
+    # Drop all tables 
     # db.drop_all()
 
 
